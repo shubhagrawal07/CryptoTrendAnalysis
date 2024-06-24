43c846f37d61b6b24e3767243f98a5fc8658cfcc
Shubh Agrawal <shubhagrawal0710@gmail.com>
2024-06-24 22:09:10 +0530

    reconfigured data

--- helper/cryptoFinder.js
+++ helper/cryptoFinder.js
@@ -1,13 +1,15 @@
-const fs = require("fs");
+// const fs = require("fs");
 
-// const filePath =
-//   "C:\\Users\\shuagrawal\\OneDrive - Conga\\Desktop\\Node\\PROJECTN037_XAVIER\\data\\words.txt";
-// const englishWords = fs.readFileSync(filePath, "utf8").split("\n");
+// // const filePath =
+// //   "C:\\Users\\shuagrawal\\OneDrive - Conga\\Desktop\\Node\\PROJECTN037_XAVIER\\data\\words.txt";
+// // const englishWords = fs.readFileSync(filePath, "utf8").split("\n");
 
-const rawData = fs.readFileSync("data\\data.json");
-const data = JSON.parse(rawData);
+// const rawData = fs.readFileSync("data\\data.json");
+// const data = JSON.parse(rawData);
 
-const englishWords = data.englishWords;
+// const englishWords = data.englishWords;
+
+const englishWords = require("../data/englishWords");
 console.log(englishWords);
 
 function isInDictionary(word) {
